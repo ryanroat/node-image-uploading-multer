b12cb088e8b03f725351202f9384a28fece06bc6
Ryan Roat <ryanroat@gmail.com>
2021-06-02 18:54:21 -0400

    add POST /upload route and upload file to local folder

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ import ejs from 'ejs';
 import multer from 'multer';
 import path from 'path';
 
+// set storage engine
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './public/uploads/');
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+// init upload
+const upload = multer({ storage });
+
 // init express server
 
 const app = express();
@@ -21,6 +34,25 @@ app.use(express.static('./public'));
 
 // render index page on GET requests to root
 app.get('/', (req, res) => res.render('index'));
+
+// test POST request to /upload
+app.post('/upload', upload.single('filename'), (req, res) => {
+  console.log(req.file, req.body);
+});
+
+// // capture POST requests to /upload
+// app.post('/upload', (req, res) => {
+//   upload(req, res, (err) => {
+//     if (err) {
+//       res.render('index', {
+//         msg: err,
+//       });
+//     } else {
+//       console.log(req);
+//       res.send('test');
+//     }
+//   });
+// });
 
 // TODO: need to catch 404 after all other paths
 
