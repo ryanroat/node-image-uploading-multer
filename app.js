ff21b6071a2cd5dee9bc7da9b2803a03648f1976
Ryan Roat <ryanroat@gmail.com>
2021-06-02 16:32:06 -0400

    add index view and route

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ const app = express();
 
 const port = 3000;
 
+// set EJS as template view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// use a public folder for static elements
+app.use(express.static('./public'));
+
+// render index page on GET requests to root
+app.get('/', (req, res) => res.render('index'));
+
+// TODO: need to catch 404 after all other paths
+
 app.listen(port, () => console.log(`Server running on port ${port}.`));
